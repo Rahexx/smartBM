3c0893d5f584523ef654b59551e4516d739aa850
Andrzej Łuszcz <endriu1297@gmail.com>
2021-01-26 21:55:03 +0100

    create Input atom and create story for this component

---
+++ src/components/atoms/Input/Input.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 137px;
+  padding: 5px 15px;
+  border-radius: 5px;
+  border: none;
+
+  @media (min-width: 400px) {
+    width: 162px;
+  }
+`;
+
+export default Input;
---
+++ src/components/atoms/Input/Input.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Input from './Input';
+
+export default {
+  title: 'Components/Atoms/Input',
+  component: Input,
+};
+
+export const mainInput = () => <Input placeholder="Nazwa kategorii" />;
