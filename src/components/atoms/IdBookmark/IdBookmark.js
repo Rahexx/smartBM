f929418e278774d30d4052e65d53a28e1ef3f92d
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-01 01:29:05 +0100

    create atom for id bookmark list, and story for this component

---
+++ src/components/atoms/IdBookmark/IdBookmark.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const IdBookmark = styled.p`
+  height: 42px;
+  width: 42px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  font-weight: bold;
+  border-radius: 50px;
+  color: #b4bf5e;
+  background-color: #242614;
+`;
+
+export default IdBookmark;
---
+++ src/components/atoms/IdBookmark/IdBookmark.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IdBookmark from './IdBookmark';
+
+export default {
+  title: 'Components/Atoms/IdBookmark',
+  component: IdBookmark,
+};
+
+export const IdBookmarkMain = () => <IdBookmark>1</IdBookmark>;
