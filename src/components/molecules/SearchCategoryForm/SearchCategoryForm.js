5843b1dfc65595bebea0d09bda43ed6e8ce59176
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-01 23:46:55 +0100

    create form to search categories bookmark and story for this compoenent

---
+++ src/components/molecules/SearchCategoryForm/SearchCategoryForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import Input from 'components/atoms/Input/Input';
+import { Search } from '@styled-icons/boxicons-regular/Search';
+
+const StyledForm = styled.form`
+  height: 40px;
+  width: 60vw;
+  position: relative;
+
+  @media (min-width: 400px) {
+    width: 55vw;
+  }
+`;
+
+const SubmitBtn = styled.button`
+  height: 27px;
+  width: 27px;
+  border: none;
+  position: absolute;
+  top: 4px;
+  right: 7px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  background-color: #b4bf5e;
+`;
+
+const SearchCategoryForm = () => (
+  <StyledForm>
+    <Input placeholder="Szukaj kategorii" search />
+    <SubmitBtn>
+      <Search />
+    </SubmitBtn>
+  </StyledForm>
+);
+
+export default SearchCategoryForm;
---
+++ src/components/molecules/SearchCategoryForm/SearchCategoryForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SearchCategoryForm from './SearchCategoryForm';
+
+export default {
+  title: 'Components/Molecules/SearchCategoryForm',
+  component: SearchCategoryForm,
+};
+
+export const searchForm = () => <SearchCategoryForm />;
