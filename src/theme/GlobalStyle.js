c5b2168f496621275e536175ecfe2b8c41f6a10d
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-02 15:24:30 +0100

    add reset to global styles

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -2,6 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     *,*::after, *::before{
+        margin: 0;
+        padding: 0;
         box-sizing: border-box;
     }
 
