c3eab775b4e42e135a7e613e67cb73f7262ff9a8
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-16 23:21:52 +0100

    add double arrow icon and function for click event for this icon

--- src/components/organisms/CategorySide/CategorySide.js
+++ src/components/organisms/CategorySide/CategorySide.js
@@ -3,13 +3,40 @@ import styled from 'styled-components';
 import SearchForm from 'components/molecules/SearchCategoryForm/SearchCategoryForm';
 import CategoryListItem from 'components/molecules/CategoryListItem/CategoryListItem';
 import AddCategoryForm from 'components/molecules/AddCategoryForm/AddCategoryForm';
+import { DoubleArrow } from '@styled-icons/material-rounded/DoubleArrow';
+
+// variables
+let isShow = true;
+
+// functions
+
+const handleClickArrow = () => {
+  const wrapper = document.querySelector('.categorySideWrapper');
+  const arrow = document.querySelector('.doubleArrow');
+
+  if (isShow) {
+    wrapper.style.transform = 'translateX(calc(-100vw + 30px))';
+    arrow.style.transform = 'translate(50%, -50%) rotate(0)';
+    isShow = !isShow;
+  } else {
+    wrapper.style.transform = 'translateX(0)';
+    arrow.style.transform = 'translate(50%, -50%) rotate(180deg)';
+    isShow = !isShow;
+  }
+};
+
+// styled elements
 
 const StyledWrapper = styled.div`
   height: 100vh;
   width: 100vw;
   padding: 25px;
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
   background-color: #485922;
+  z-index: 2;
+  transition: 0.5s;
 `;
 
 const StyledHeader = styled.h2`
@@ -24,8 +51,16 @@ const ListCategory = styled.ul`
   list-style: none;
 `;
 
+const StyledDoubleArrowIcon = styled(DoubleArrow)`
+  position: absolute;
+  top: 50vh;
+  right: 5vw;
+  transform: translate(50%, -50%) rotate(180deg);
+  color: white;
+`;
+
 const CategorySide = () => (
-  <StyledWrapper data-testid="CategorySide-element">
+  <StyledWrapper className="categorySideWrapper" data-testid="CategorySide-element">
     <SearchForm />
     <StyledHeader>Kategorie</StyledHeader>
     <ListCategory>
@@ -34,6 +69,7 @@ const CategorySide = () => (
       <CategoryListItem name="Filmiki" />
     </ListCategory>
     <AddCategoryForm />
+    <StyledDoubleArrowIcon className="doubleArrow" size="30" onClick={handleClickArrow} />
   </StyledWrapper>
 );
 
