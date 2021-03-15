bdb7200add2c14f6f9393ed8fdf1d739c09e7159
Andrzej Łuszcz <endriu1297@gmail.com>
2021-03-15 22:42:05 +0100

    add localStorage to app for save data in browser

--- src/views/MainView/MainView.js
+++ src/views/MainView/MainView.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable consistent-return */
 /* eslint-disable array-callback-return */
 import React, { useState } from 'react';
@@ -7,33 +8,18 @@ import AddBookmark from 'components/molecules/AddBookmark/AddBookmark';
 import BookmarksSide from 'components/organisms/BookmarksSide/BookmarksSide';
 import CategorySide from 'components/organisms/CategorySide/CategorySide';
 
-const initialStateCategories = [
-  {
-    name: 'Gry',
-    bookmarks: [
-      {
-        title: 'Witcher',
-        description:
-          ' fabularna gra akcji wyprodukowana i wydana przez CD Projekt Red 19 maja 2015 na platformy Microsoft Windows, PlayStation 4 i Xbox One. Gra jest kontynuacją wydanego w 2007 roku Wiedźmina oraz Wiedźmina 2: Zabójców królów, który miał swoją premierę w 2011 roku.',
-        link: 'https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/',
-      },
-      {
-        title: 'Civilization VI',
-        description:
-          'komputerowa strategiczna gra turowa stworzona przez Firaxis Games i wydana przez 2K Games 21 października 2016 roku na platformę Microsoft Windows oraz 9 lutego 2017 na system Linux',
-        link: 'https://www.epicgames.com/store/pl/p/sid-meiers-civilization-vi',
-      },
-    ],
-  },
-];
-
 const MainView = () => {
-  const [categoryStore, setCategoryStore] = useState(initialStateCategories);
+  const initialStateCategories = JSON.parse(localStorage.getItem('categoryStore'));
+  const [categoryStore, setCategoryStore] = useState(
+    initialStateCategories.length ? initialStateCategories : [],
+  );
+
   const [active, setActive] = useState('');
 
   const addCategory = (name) => {
     const updateStore = [...categoryStore, { name, bookmarks: [] }];
     setCategoryStore(updateStore);
+    localStorage.setItem('categoryStore', JSON.stringify(updateStore));
   };
 
   const changeActive = (nameCategory) => {
@@ -56,6 +42,7 @@ const MainView = () => {
     }
 
     setCategoryStore([...newStore]);
+    localStorage.setItem('categoryStore', JSON.stringify([...newStore]));
   };
 
   const deleteBookmark = (id) => {
@@ -68,6 +55,7 @@ const MainView = () => {
     }
 
     setCategoryStore([...newStore]);
+    localStorage.setItem('categoryStore', JSON.stringify([...newStore]));
   };
 
   const contextValue = {
